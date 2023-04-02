68e0c8cd8e5048c8f51e068e7f0be69ddf0ef25c
santiagomanso <santiagomanso@web.de>
2023-04-02 17:32:07 +0200

    🔧 reworked App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import Navbar from './components/Navbar';
+import Container from './components/Container';
+import Footer from './components/Footer';
+import Cart from './components/Cart';
+import { CartProvider } from './context/CartContext';
+import { LanguageProvider } from './context/LanguageContext';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CartProvider>
+      <LanguageProvider>
+        <Container>
+          <Navbar />
+          <Cart />
+          <Footer />
+        </Container>
+      </LanguageProvider>
+    </CartProvider>
   );
-}
+};
 
 export default App;
